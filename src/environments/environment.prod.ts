1947b40da3fad57df253f9d0289b57f04c62710a
hock <tehboonhock@hotmail.com>
2023-10-28 04:52:26 +0800

    add missing file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  serverUrl: `https://hockfmnestjs.azurewebsites.net`,
+  getNewChannelsUrl: `https://hockfm.000webhostapp.com/req/get-new-channels.php`,
+  getNewPlaylistsUrl: `https://hockfm.000webhostapp.com/req/get-new-playlists.php`,
+  getNewPodcastsUrl: `https://hockfm.000webhostapp.com/req/get-new-podcasts.php`,
+};
